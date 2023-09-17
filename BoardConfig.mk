b4eabd9232caa4c69af24ea744194da366e045a3
fuxi14 <76251359+fuxi14@users.noreply.github.com>
2023-09-17 22:49:37 +0200

    yamcha: BoardConfig.mk: Updated TARGET_RECOVERY_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,7 +191,7 @@ BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 TARGET_RIL_VARIANT := caf
