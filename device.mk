31e1cd16bbd5ac190d7ef9d79664ab700bf62850
fuxi14 <76251359+fuxi14@users.noreply.github.com>
2023-09-17 21:35:47 +0200

    yamcha: device.mk: Add it from chaozu

---
+++ device.mk
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/bq/yamcha/yamcha-vendor.mk)
+
+# Init
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+# The following is what rests from device.mk from android_device_bq_chaozu on lineage OS
+
+# # Camera
+# PRODUCT_COPY_FILES += \
+#     $(LOCAL_PATH)/configs/camera/camera_config.xml:system/etc/camera/camera_config.xml \
+#     $(LOCAL_PATH)/configs/camera/ov5675_d5v15b_chromatix.xml:system/etc/camera/ov5675_d5v15b_chromatix.xml \
+#     $(LOCAL_PATH)/configs/camera/s5k3l8_chromatix.xml:system/etc/camera/s5k3l8_chromatix.xml \
+#     $(LOCAL_PATH)/configs/camera/s5k3l8_f3l8yam_chromatix.xml:system/etc/camera/s5k3l8_f3l8yam_chromatix.xml \
+#     $(LOCAL_PATH)/configs/camera/s5k3l8_mono_chromatix.xml:system/etc/camera/s5k3l8_mono_chromatix.xml
+#
+# # IRQ Balancer
+# PRODUCT_COPY_FILES += \
+#     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+#
+# # NFC
+# PRODUCT_PACKAGES += \
+#     com.android.nfc_extras \
+#     com.nxp.nfc.nq \
+#     nfc_nci.nqx.default \
+#     NQNfcNci \
+#     nqnfcee_access.xml \
+#     nqnfcse_access.xml \
+#     Tag
+#
+# PRODUCT_COPY_FILES += \
+#     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+#     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+#
+# # Permissions
+# PRODUCT_COPY_FILES += \
+#     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+#     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+#     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
